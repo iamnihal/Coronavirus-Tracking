0abd80fdbd05bad21f94c855febdc358d0e4006a
Nihal <37813784+iamnihal@users.noreply.github.com>
2020-03-16 12:02:41 +0530

    Update corona.sh

--- corona.sh
+++ corona.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 clear
+if [ ! -x "$(command -v notify-send)" ]
+then
+    echo "[-] This script requires notify-send. Exiting."
+    exit 1
+fi
 JSON=$(curl -sL 'https://coronavirus-tracker-api.herokuapp.com/all')
 INFECT=$(echo "$JSON" | jq -r ".latest.confirmed")
-sleep 2.5
-notify-send -t 10000 -i face-worried Infected:- "$INFECT"
+sleep 2
+notify-send -t 10500 -i face-worried Infected:- "$INFECT"
 sleep 2.5
 DEATH=$(echo "$JSON" | jq -r ".latest.deaths")
-notify-send -t 10000 -i face-sad Deaths:- "$DEATH"
+notify-send -t 10500 -i face-sad Deaths:- "$DEATH"
 sleep 2.5
 RECOVER=$(echo "$JSON" | jq -r ".latest.recovered")
-notify-send -t 10000 -i face-angel Recovered:- "$RECOVER"
+notify-send -t 10500 -i face-angel Recovered:- "$RECOVER"
